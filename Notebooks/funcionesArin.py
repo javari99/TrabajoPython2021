e9f86bca6b7a158785cd29c48cac8b8474c64487
javari99 <javierarin99@gmail.com>
2021-04-16 00:54:11 +0200

    Refactorizado código (full spaguetti)

--- Notebooks/funcionesArin.py
+++ Notebooks/funcionesArin.py
@@ -1,14 +1,17 @@
 #Funciones de Arin
+from babel.numbers import parse_decimal
+import regex as re
+
 
 def twoSum(arr : list, target : float) -> list:
     """[summary]
-    Busca todos los pares de números de la lista que suman al target
+    Busca todos los pares de números de la lista que suman al target\n
     Args:
-        arr (list): [Lista de números]
-        target (float): [description]
+        arr (list): Lista de números\n
+        target (float): Numero deseado de suma\n
 
     Returns:
-        list: [description]
+        [list] : Lista con todos los candidatos que cumplen con la suma\n
     """
     matches = set()
     for x in arr:
@@ -18,32 +21,68 @@ def twoSum(arr : list, target : float) -> list:
                     matches.add((x,y))
     return list(matches)
 
+
 def twoSumGetMostLikely(arr : list, target : float) -> tuple:
     """[summary]
     Busca todos los pares de números de la lista que suman target y filtra aquellos 
-    que no vayan a ser el precio final
+    que no vayan a ser el precio final\n
     Args:
-        arr (list): [Lista de numeros por los que se buscan el target]
-        target (float): [El número deseado que encontrar con la suma]
+        arr (list): Lista de numeros por los que se buscan el target\n
+        target (float): El número deseado que encontrar con la suma\n
 
     Returns:
-        tuple: [Los dos candidatos para el precio]
+        [tuple] : Los dos candidatos para el precio\n
     """
     candidates = twoSum(arr,target)
-    #Buscamos el máximo de todos, que probablemente sea la base imponible
+    # Buscamos el máximo de todos, que probablemente sea la base imponible
     max = -1
     for i in candidates:
         for el in i:
             max = el if el > max else max
-    
+
+    # Devolvemos el que tenga el maximo
     for i in candidates:
         if max in i:
             return i
     
 
-
-
-def sumaivas(arr: list, target: float) -> list:
+def sumaIVAS(arr: list, target: float) -> list:
     matches = []
     IVAVALUES = [0, 0.04, 0.1, 0.105, 0.106, 0.21]
     pass
+
+
+def findNIF(instring):
+    """[summary]
+    Funcion que busca un NIF dentro de un string\n
+    Args:
+        instring (string) : String en el que buscar el NIF\n
+    Returns:
+        [list]: Lista con todos los NIF del fichero\n
+    """
+    matches = re.findall('([A-Z]-?\d{8}|\d{8}[-| ]?[A-Z]|[A-Z]-?\d{10}|\d{10}[-| ]?[A-Z])\s', \
+                instring)
+    return matches
+
+
+def getAllPricesAndTotal(data : str) -> tuple:
+    #Recogemos el precio total de la factura
+    matches = re.findall('(?:\d+?\.?)(?:\d*\,\d{2})', data)
+    #convertimos a numeros y luego filtramos el maximo
+    matches = [float(parse_decimal(d, locale="es_ES")) for d in matches]
+    
+    if matches:
+        # Filtremos para quitar los 0 de la lista
+        matches = list(filter(lambda a: a>0, matches))
+
+    #Si no ha detectado ningun precio probablemente sea que el numero esta en ingles
+    else:
+        matches = re.findall('(?:\d+?\,?)(?:\d*\.\d{2})', data)
+        #print(f"DEBUG: <{filename}> : \n\r{matches}\n\r")
+        matches = [float(parse_decimal(d, locale="en_US")) for d in matches]
+        # Filtramos para quitar los 0 de la lista
+        matches = list(filter(lambda a: a>0, matches))
+
+    maximum = max(matches)
+    # Devolvemos la lista de todos los precios encontrados y el del máximo
+    return (matches, maximum)
